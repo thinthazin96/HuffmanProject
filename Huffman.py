b1752bac62a659541611f1892afffa973027cf9c
Thin Thazin <thinthazin@imac.myfiosgateway.com>
2021-11-13 19:23:55 -0500

    Count freqency of value

--- Huffman.py
+++ Huffman.py
@@ -2,3 +2,17 @@
 #CSCI 375
 #11/13/2021
 
+
+a_list = input()
+
+frequencies = {}
+for item in a_list:
+    if item in frequencies:
+        frequencies[item] += 1
+    else:
+        frequencies[item] = 1
+
+print(frequencies)
+
+for key in sorted(frequencies):
+    print(key,frequencies[key])
